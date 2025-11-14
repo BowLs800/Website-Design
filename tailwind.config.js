7addcb1f5bf66cabb5a82747fa1c0c95f259ba9d
BowLs800 <bowman@ledstar.com>
2025-11-14 15:39:31 -0500

    Upload full updated website

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  content: [
+    "./*.html",              // 扫描根目录HTML（mainUp.html）
+    "./src/**/*.{html,js}",  // 扫描 src 文件夹
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
